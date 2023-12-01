6ecc588148b7232db7c533bd27bc71d59671b677
Dmytro Myronenko <dim@uain.su>
2023-12-01 11:48:46 +0200

    added new files

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import {Text, View} from 'react-native';
+import Title from '../../components/Title';
+
+const Home = () => {
+  const [title, setTitlte] = useState('My Home');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTitlte('Updated from timeout');
+    }, 3000);
+  });
+
+  console.log('title => ', title);
+
+  return (
+    <View>
+      <Text>
+        <Title text={'Text from home'} />
+      </Text>
+    </View>
+  );
+};
+
+export default React.memo(Home);
